2f3851ecaa75514106c41178e5d0f131cfdb7301
Aleksandar Kinanov <akinanov@Aleksandars-MacBook-Pro.local>
2022-12-03 18:16:42 +0200

    Adds day 3 tasks

--- src/day3-task1.rs
+++ src/day3-task1.rs
@@ -1,20 +1,20 @@
-use std::{fs, cmp};
+use std::fs;
 
 fn main() {
     let mut result = 0;
     let data = fs::read_to_string("./dummy_data.txt").expect("Unable to read file");
     let split_data: Vec<&str> = data.split("\n").into_iter().collect();
 
-    for item in split_data.chunks(3) {
-        let thing: Vec<char> = item[0].chars().filter(|a| item[1].contains(*a)).filter(|a| item[2].contains(*a)).collect();
+    for item in split_data {
+        let len = item.len();
+        let first_part = item.split_at(len/2);
+        let thing: Vec<char> = first_part.0.chars().filter(|a| first_part.1.contains(*a)).collect();
         let parsed = match thing[0].is_uppercase() {
             true => thing[0] as u32 - 38,
             false => thing[0] as u32 - 96
         };
-        println!("{:?}", thing[0]);
+        
         result += parsed;
     }
-
     println!("{:?}", result);
-
 }
--- src/day3-task2.rs
+++ src/day3-task2.rs
@@ -1,4 +1,4 @@
-use std::{fs, cmp};
+use std::fs;
 
 fn main() {
     let mut result = 0;
@@ -6,15 +6,16 @@ fn main() {
     let split_data: Vec<&str> = data.split("\n").into_iter().collect();
 
     for item in split_data.chunks(3) {
-        let thing: Vec<char> = item[0].chars().filter(|a| item[1].contains(*a)).filter(|a| item[2].contains(*a)).collect();
+        let thing: Vec<char> = item[0]
+            .chars()
+            .filter(|a| item[1].contains(*a))
+            .filter(|a| item[2].contains(*a))
+            .collect();
         let parsed = match thing[0].is_uppercase() {
             true => thing[0] as u32 - 38,
-            false => thing[0] as u32 - 96
+            false => thing[0] as u32 - 96,
         };
-        println!("{:?}", thing[0]);
         result += parsed;
     }
-
     println!("{:?}", result);
-
 }
