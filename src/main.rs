42721b09fee7a47e4cba0f2e4a7fe3ea1aae7982
Aleksandar Kinanov <aleksandar.kinanov@kiteworks.com>
2022-12-21 12:56:39 +0200

    Adds day 9

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,49 @@
-// use std::io::Read;
+use std::collections::HashSet;
+use itertools::Itertools;
+
+#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug)]
+struct Point{
+    x: i32,
+    y: i32,
+}
 
 fn main() {
+    let start = Point { x: 0, y: 0 };
+    // let mut tail = Point { x: 0, y: 0 };
+    let mut marked:HashSet<Point> = HashSet::new();
+    let mut rope = vec![start; 10];
     let input = include_str!("dummy_data.txt");
+    marked.insert(start);
+    let mut action_x = 0;
+    let mut action_y = 0;
 
-    let grid: Vec<Vec<_>> = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| c.to_digit(10).unwrap() as i8)
-                .collect()
-        })
-        .collect();
-
-    let height = grid.len();
-    let width = grid[0].len();
-    println!(
-        "{}",
-        grid.iter()
-            .enumerate()
-            .flat_map(|(tree_position_y, row)| row
-                .iter()
-                .enumerate()
-                .map(move |(tree_position_x, tree_height)| (tree_position_y, tree_position_x, tree_height)))
-            .map(|(tree_position_y, tree_position_x, tree_height)| {
-                let score = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-                    .iter()
-                    .map(|(next_tree_position_y, next_tree_position_x)| {
-                        let (mut tree_position_y, mut tree_position_x) = (tree_position_y as isize, tree_position_x as isize);
-                        let mut visible_trees = 0;
-                        let mut last_lower_tree = 0;
-                        loop {
-                            println!("{} {}", tree_position_y, tree_position_x);
-                            tree_position_y += next_tree_position_y;
-                            tree_position_x += next_tree_position_x;
-                            // if we hit an edge
-                            if tree_position_y < 0 || tree_position_x < 0 || tree_position_y as usize >= height || tree_position_x as usize >= width {
-                                break;
-                            }
-                            visible_trees += 1;
-                            let next_visible_tree = grid[tree_position_y as usize][tree_position_x as usize];
-                            if next_visible_tree > last_lower_tree {
-                                if next_visible_tree >= *tree_height {
-                                    break;
-                                }
-                                last_lower_tree = next_visible_tree;
-                            }
-                        }
-                        visible_trees
-                    })
-                    .product::<u32>();
-                score
-            })
-            .max()
-            .unwrap()
-    );
+    for line in input.lines(){
+        let (dir, steps) = line.split_once(' ').unwrap();
+        for _ in 0..steps.parse::<i32>().unwrap(){
+            action_x = 0;
+            action_y = 0;
+            match dir{
+                "R" => {rope[0].y += 1; action_y = 1;},
+                "L" => {rope[0].y -= 1; action_y = -1;},
+                "U" => {rope[0].x += 1; action_x = 1;},
+                "D" => {rope[0].x -= 1; action_x = -1;},
+                _ => panic!("Invalid direction"),
+            }
+    
+            for (head, tail) in (0..rope.len()).tuple_windows() {
+                let not_adjacent = (rope[head].x - rope[tail].x).abs() > 1 || (rope[head].y - rope[tail].y).abs() > 1;
+                if not_adjacent {
+                    rope[tail].x += action_x;
+                    rope[tail].y += action_y;
+                    if tail == rope.len() - 1 {
+                        marked.insert(rope[rope.len() - 1]);
+                    }
+                }
+                
+            }
+            
+            
+        }
+    }
+    println!("{:#?}", marked.len());
 }
